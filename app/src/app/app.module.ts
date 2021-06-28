a3da23f54d37790644378f7963d1cf7b0ca670fe
Samuel <samuel.rodier.dev@gmail.com>
2021-06-28 15:59:30 +0200

    feat() Création d'un nouveau composant Angular liste de livres.

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksListComponent } from './books-list/books-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksListComponent
   ],
   imports: [
     BrowserModule,
